b758b94920a3a1c8020caa7be27b36591d3dfb47
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2024-06-26 11:06:49 +0300

    feat: solve task to create a shopping cart with goods

--- homeWork6/script.js
+++ homeWork6/script.js
@@ -16,7 +16,7 @@ async function fetchData(url) {
     console.log(error.message);
   }
 }
-
+const cartContent = document.querySelector(".cart-content");
 document.addEventListener("DOMContentLoaded", async () => {
   const data = await fetchData(url1);
 
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   data.forEach(el => {
 
     cardsEl.insertAdjacentHTML("beforeend", `
-    <div class="cards__item">
+    <div class="cards__item" id="${el.id}">
          <div class="item-img" style="background-image: url(${el.url})">
           <div class="item-img__hover">
             <button class="item-img__hover-btn">Add to Cart</button>
@@ -38,9 +38,82 @@ document.addEventListener("DOMContentLoaded", async () => {
             <p class="item-price">${el.price}</p>
           </div>
     </div>
-  
     `)
   });
+
+updateCart(data);
 })
+
+const cartIcon = document.querySelector('#cart-icon');
+const cart = document.querySelector('.cart');
+const cartClose = document.querySelector('#close-cart');
+
+// let itemDeletes = document.querySelectorAll('.cart-remove');
+// console.log(itemDeletes);
+//
+// for (const element of itemDeletes) {
+//   element.addEventListener('click', removeCartItem);
+// }
+
+cartIcon.addEventListener('click', () => {
+  cart.classList.add('active');
+});
+
+cartClose.addEventListener('click', () => {
+  cart.classList.remove('active');
+});
+
+function removeCartItem (cartItem, data) {
+  let parent = cartItem.parentElement;
+  localStorage.removeItem(parent.id);
+  updateCart(data);
+}
+
+function updateCart(data){
+
+  const productsCards = document.querySelectorAll(".cards__item");
+  cartContent.innerHTML = '';
+  productsCards.forEach((element) => {
+    element.addEventListener("click", (evt) => {
+      if (evt.target.classList.contains("item-img__hover-btn")) {
+        data.forEach((el) => {
+          if (el.id == element.id) {
+            localStorage.setItem(JSON.stringify(el.id), [
+              JSON.stringify(el.title),
+              JSON.stringify(el.price),
+              JSON.stringify(el.url),
+            ]);
+
+
+            cartContent.insertAdjacentHTML(
+                "afterbegin",
+                `<div class="cart-box" id="${el.id}">
+              <img class="cart-img" src="${el.url}" alt="">
+              <div class="detail-box">
+                <div class="item-title">${el.title}</div>
+                <div class="item-price">${el.price}</div>
+                <input type="number" value="1" min="1" class="cart-quantity">
+              </div>
+              <i class="fa fa-regular fa-trash cart-remove""></i>
+            </div>`
+            );
+            let itemDeletes = document.querySelectorAll('.cart-remove');
+            console.log(itemDeletes);
+
+            for (const cartItem of itemDeletes) {
+              cartItem.addEventListener('click', (evt) => {
+                let parent = cartItem.parentElement;
+                localStorage.removeItem(parent.id);
+                cartItem.parentElement.remove();
+              });
+            }
+          }
+        });
+      }
+    });
+  });
+}
+
+
 
 
